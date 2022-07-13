8e757e0ebbecae097d4492c58a741c122f9e7361
James Perkins <jamesperkins@hey.com>
2022-07-12 21:00:08 -0400

    add some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://marvelous-chaja-b4437a.netlify.app',
+  syntaxHighlight: 'prism',
   integrations: [react(), sitemap(), mdx(), tailwind()]
 });
