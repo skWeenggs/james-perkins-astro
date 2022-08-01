094f66be31e955eaddef8b6683bec868e88aa1fd
James Perkins <jamesperkins@hey.com>
2022-08-01 19:06:21 -0400

    Optimize tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import { astroImageTools } from 'astro-imagetools';
 
 import tailwind from '@astrojs/tailwind';
 
@@ -9,5 +10,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://jamesperkins.dev',
   syntaxHighlight: 'prism',
-  integrations: [react(), sitemap(), mdx(), tailwind()]
+  integrations: [react(), sitemap(), mdx(), tailwind(), astroImageTools]
 });
